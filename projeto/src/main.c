818370c865bd095ba1d73ed5803bb563ee2528f5
Arthur Pereira <132160253+ArthuPereira@users.noreply.github.com>
2025-12-04 21:00:28 -0300

    test: implementação da função limparTela() para teste do Makefile + deletar o 'jogo' só pra limpar minhas staged changes

---
+++ projeto/src/main.c
@@ -0,0 +1,8 @@
+#include "../include/screen.h"
+#include <stdlib.h>
+
+int main()
+{
+	limparTela();
+	return EXIT_SUCCESS;
+}
---
+++ projeto/src/screen.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+void limparTela()
+{
+    system("clear");
+}
