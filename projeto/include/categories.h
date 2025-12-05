23d4bdae4547b0282d1ad2ef846a4f3edf719587
Arthur Pereira <132160253+ArthuPereira@users.noreply.github.com>
2025-12-05 17:28:33 -0300

    feat: implementações de categories + docs

--- projeto/include/categories.h
+++ projeto/include/categories.h
@@ -10,7 +10,21 @@ typedef enum {
     CAT_TOTAL
 } Categoria;
 
+/**
+ * int usadas[].
+ * 0 -> livre, count-1 -> usada
+ *
+ * Em disponiveis[i] estará o número equivalente a categoria.
+ * Ex: usadas[1, 0, 1, 0, 0]  -> disponiveis[1, 3, 4, 0, 0]
+ *
+ * Obs: se o retorno for 5 (CAT_TOTAL) não há categorias disponiveis
+ */
 Categoria sortearCategoria(int usadas[]);
+
+/**
+ * Obter o nome de uma categoria a partir do seu número equivalente.
+ * Ex: nomeCategoria[2] -> "Nome de Animal"
+ */
 const char* nomeCategoria(Categoria c);
 
 #endif
