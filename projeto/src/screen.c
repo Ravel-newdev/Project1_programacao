7f4610f26d0b1e8437e68081c1d8b5ee7f242e60
Mateus Nasc <105395333+Mateus-Nasc@users.noreply.github.com>
2025-12-06 00:58:04 -0300

    implementando função limpar tela e banner

--- projeto/src/screen.c
+++ projeto/src/screen.c
@@ -1,6 +1,26 @@
-#include <stdlib.h>
 
-void limparTela()
-{
-    system("clear");
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/screen.h"
+
+// Função responsável por limpar o terminal
+// usando diretivas de pré-processador para identificar o SO
+void limparTela() {
+    //verifica se é Windows _WIN32 ou Linux/Unix
+    #if defined(_WIN32) || defined(_WIN64)
+        system("cls");
+    #else
+        system("clear"); // para sistemas baseados em Unix
+    #endif
+}
+
+// banner do jogo
+void exibirBannerInicial() {
+    printf("\n");
+    printf("****************************************\n");
+    printf("* A M E D O N H A            *\n");
+    printf("* Trabalho Intermediario        *\n");
+    printf("****************************************\n");
+    printf("\n");
 }
